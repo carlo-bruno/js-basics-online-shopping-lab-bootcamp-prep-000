743bb59a85ca436fa48b8757ef239df89766d452
Carlo Bruno <carlofbruno@gmail.com>
2018-11-09 22:23:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,9 +45,11 @@ function removeFromCart(item) {
     if (item === cartItem.itemName) {
       cart.splice(i,1);
       return cart;
-    } 
-      return "That item is not in your cart.";
+    } else {
+      
+    }
   })
+  
   
 }
 
